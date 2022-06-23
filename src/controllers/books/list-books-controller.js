cfc54e6979a1319064957809d61dd591e3f479a2
Gisela Lanciani <giselalanciani@gmail.com>
2022-06-23 17:43:48 -0300

    adding link

--- src/controllers/books/list-books-controller.js
+++ src/controllers/books/list-books-controller.js
@@ -1,6 +1,10 @@
 class ListBooksController {
   constructor() {
-    console.log("books page");
+    const createButton = document.getElementById("create-button");
+    createButton.addEventListener("click", this.onClickCreateButton);
   }
+  onClickCreateButton = () => {
+    window.location.href = "/books/create";
+  };
 }
 const booksCtrl = new ListBooksController();
