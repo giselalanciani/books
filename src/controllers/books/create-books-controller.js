ca29a8972a6051c7901115b546f8823ad9fe2d03
Gisela Lanciani <giselalanciani@gmail.com>
2022-06-23 17:36:48 -0300

    post new books

--- src/controllers/books/create-books-controller.js
+++ src/controllers/books/create-books-controller.js
@@ -1,7 +1,58 @@
+/**
+ * 1) El boton salvar
+ *      Al hacer click ->
+ *          1) Validacion
+ *              1) Debe hacer un POST a la API de books
+ *              2) Muestra un alert con un mejaje que diga "Se guardo el libro".
+ *              3) Debe redireccionar a la location de /books
+ *          2) Si la validacion no es correcta
+ *              1) muestra un mejs con el error.
+ *
+ * 1.b Cómo?
+ *      1) Buscamos un boton por su nombre con document.querySelector()  "click"
+ *      2) A ese boton encontrado, le asignamos una funcion de event Handler
+ *          2.1) esta funcion, va a ser un methodo de la clase controller
+ *          2.2) Meter un console log para saber que funciona.
+ *
+ *
+ *
+ *
+ *
+ */
 class CreateBooksController {
-    constructor() {
-        console.log("create page");
+  constructor() {
+    const createBookButton = document.getElementById("create-book-button");
+    createBookButton.addEventListener("click", this.onClickCreateBookButton);
+  }
+
+  onClickCreateBookButton = () => {
+    console.log("hizo click");
+    if (this.sendData() === true) {
+    } else {
+      console.log("NO ENVIAMOS DATA");
     }
+  };
+
+  sendData = async () => {
+    console.log("enviamos la informacion");
+
+    const bookName = document.querySelector("[name='bookname']");
+    const bookYear = document.querySelector("[name='year']");
+
+    const rawResponse = await fetch("http://localhost:3000/api/book", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: bookName.value, year: bookYear.value }),
+    });
+    const content = await rawResponse.json();
+
+    console.log(content);
+    alert("Libro creado");
+    window.location.href = "/books";
+  };
 }
 
-const createCtrl = new CreateBooksController;
+const createCtrl = new CreateBooksController();
