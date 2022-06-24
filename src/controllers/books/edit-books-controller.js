9989e27ef3c33c327e2254c96c3cf5f03b892ff5
Gisela Lanciani <giselalanciani@gmail.com>
2022-06-24 10:03:28 -0300

    adding save edit button to work

--- src/controllers/books/edit-books-controller.js
+++ src/controllers/books/edit-books-controller.js
@@ -1,7 +1,36 @@
 class EditBooksController {
-    constructor() {
-        console.log("edit section");
-    }
+  constructor() {
+    const saveButton = document.getElementById("save-book-button");
+    saveButton.addEventListener("click", this.onClickSaveButton);
+  }
+
+  getQueryParams() {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    return params;
+  }
+
+  onClickSaveButton = async (event) => {
+    const bookNameInput = document.querySelector("[name='bookname']");
+    const bookYearInput = document.querySelector("[name='year']");
+
+    const saveButton = await fetch(
+      `http://localhost:3000/api/book/${this.getQueryParams().id}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: bookNameInput.value,
+          year: bookYearInput.value,
+        }),
+      }
+    );
+    alert("libro fue guardado correctamente");
+    window.location.href = "/books";
+  };
 }
 
-const editCtrl = new EditBooksController;
+const editCtrl = new EditBooksController();
