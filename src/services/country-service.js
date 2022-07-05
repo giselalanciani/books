27ac3e0c6b5efaa00064cb94e64331e15f818c57
Gisela Lanciani <giselalanciani@gmail.com>
2022-07-05 15:25:16 -0500

    create country services

--- src/services/country-service.js
+++ src/services/country-service.js
@@ -5,45 +5,42 @@ class CountryServices {
     return await response.json();
   }
 
-  //   async deleteCountry(id) {
-  //     const response = await fetch(`http://localhost:3000/api/countries/${id}`, {
-  //       method: "DELETE",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //     });
-  //     return await response.json();
-  //   };
+  async deleteCountry(id) {
+    const response = await fetch(`http://localhost:3000/api/country/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    });
+    return await response.json();
+  }
 
-  //   async createCountry(country) {
-  //     const response = await fetch("http://localhost:3000/api/country", {
-  //       method: "POST",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         name: country.name,
+  async createCountry(country) {
+    const response = await fetch(`http://localhost:3000/api/country`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(country),
+    });
+    return await response.json();
+  }
 
-  //       }),
-  //     });
-  //     return response;
-  //   }
-  //   async updateCountry(id, country) {
-
-  //     const response = await fetch(`http://localhost:3000/api/country/${id}`, {
-  //       method: "PUT",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         name: country.name,
-
-  //       }),
-  //     });
-  //     return response;
-  //   }
+  async updateCountry(country) {
+    const response = await fetch(
+      `http://localhost:3000/api/country/${country.id}`,
+      {
+        method: "PUT",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(country),
+      }
+    );
+    return await response.json();
+  }
 }
 export { CountryServices };
